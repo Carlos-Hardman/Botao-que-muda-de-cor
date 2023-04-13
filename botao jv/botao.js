d0d04db6e23e1c1aad2eeb91e4b5ebd0a5eeb7fa
Carlos Hardman <carloshardmanjr@hotmail.com>
2023-04-13 10:17:35 -0300

    botão que faz a letra mudar de cor

---
+++ botao jv/botao.js
@@ -0,0 +1,9 @@
+function colorlaranja(){
+	document.getElementById("mudacor").style.color="orange";
+}
+function colorazul(){
+	document.getElementById("mudacor").style.color="blue";
+}
+function colorvermelho(){
+	document.getElementById("mudacor").style.color="red";
+}
